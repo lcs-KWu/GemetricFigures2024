eb397e9c8523b281af25b5f229a339c3d40118c3
Kevin Wu <KWu@lcs.on.ca>
2024-10-03 08:52:28 -0400

    Initial Commit

---
+++ GemetricFigures/GemetricFiguresApp.swift
@@ -0,0 +1,17 @@
+//
+//  GemetricFiguresApp.swift
+//  GemetricFigures
+//
+//  Created by Yixuan Wu on 2024-10-03.
+//
+
+import SwiftUI
+
+@main
+struct GemetricFiguresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
